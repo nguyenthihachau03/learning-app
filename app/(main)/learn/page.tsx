f2909194e39fed89882d10892ef6316da45b4bae
nguyenthihachau03 <146950113+nguyenthihachau03@users.noreply.github.com>
2025-02-01 22:35:17 +0700

    course progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,26 +1,39 @@
+import { redirect } from "next/navigation";
+
+import { lessons, units as unitsSchema } from "@/db/schema";
 import { title } from "process";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
 import { Header } from "./header";
-import { getUnits, getUserProgress } from "@/db/queries";
-import { redirect } from "next/navigation";
+import { getCourseProgress, getLessonPercentage, getUnits, getUserProgress } from "@/db/queries";
 import { Unit } from "./unit";
+
 
 const LearnPage = async () => {
 
     const userProgressData = getUserProgress();
+    const courseProgressData = getCourseProgress();
+    const lessonPercentageData = getLessonPercentage();
     const unitsData = getUnits();
 
     const [
         userProgress,
         units,
+        courseProgress,
+        lessonPercentage,
     ] = await Promise.all([
         userProgressData,
         unitsData,
+        courseProgressData,
+        lessonPercentageData,
     ]);
 
     if (!userProgress || !userProgress.activeCourse) {
+        redirect("/courses");
+    }
+
+    if (!courseProgress) {
         redirect("/courses");
     }
 
@@ -46,8 +59,10 @@ const LearnPage = async () => {
                                 description={unit.description}
                                 title={unit.title}
                                 lessons={unit.lessons}
-                                activeLesson={undefined}
-                                activeLessonPercentage={0}
+                                activeLesson={courseProgress?.activeLesson as typeof lessons.$inferSelect & {
+                                    unit: typeof unitsSchema.$inferSelect;
+                                } | undefined}
+                                activeLessonPercentage={lessonPercentage}
                             />
                         </div>
                     ))
