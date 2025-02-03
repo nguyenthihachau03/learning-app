b6f05e442ddd20f2869f1a78ab82b231de50598a
nguyenthihachau03 <146950113+nguyenthihachau03@users.noreply.github.com>
2025-02-03 23:42:16 +0700

    challenge footer

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { useCallback } from "react";
+import {useAudio, useKey} from "react-use";
 
 import { challenges } from "@/db/schema";
 import { cn } from "@/lib/utils";
@@ -28,9 +30,20 @@ export const Card = ({
     status,
     type,
 }: Props) => {
+    const [audio, _, controls] = useAudio({ src: audioSrc || "" });
+
+    const handleClick = useCallback(() => {
+        if(disable) return;
+
+        controls.play();
+        onClick();
+    }, [disable, onClick, controls]);
+
+    useKey(shortcut, handleClick, {}, [handleClick]); //shortcut cua element card.tsx
+
     return (
         <div
-            onClick={() => {}}
+            onClick={handleClick}
             className={cn(
                 "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
                 selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
@@ -40,6 +53,7 @@ export const Card = ({
                 type === "ASSIST" && "lg:p-3 w-full"
             )}
         >
+            {audio}
             {imageSrc && (
                 <div
                     className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full"
